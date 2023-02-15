ea2ea49d6bc0be12d5aa46c389b65f3a178aa002
abhishek <aksharma.27mjm@gmail.com>
2023-02-16 01:10:46 +0530

    mongodb wrong id error handled

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -4,6 +4,14 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+
+    //WRONG MONGODB ID ERROR
+    if(err.name ==="CastError"){
+        const msg = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(msg, 400);
+    }
+
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
